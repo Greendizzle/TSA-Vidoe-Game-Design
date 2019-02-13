0cc2d19b47a09ca3455b8bc7e594a35e9af9e17d
CHANGSAW <0texasstate0@protonmail.com>
2019-02-12 23:11:12 -0600

    Basically started on text boxes

---
+++ TSA Vidoe Game Design/Assets/DialogueManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentence = new Queue<string>();
+        
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+    }
+
+}
+    
