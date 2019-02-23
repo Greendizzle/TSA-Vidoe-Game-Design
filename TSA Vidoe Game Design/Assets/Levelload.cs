fd5e69dad65f45c2640aa7b66aff586671fa92dd
Greendizzle <greendizzle2@gmail.com>
2019-02-23 17:07:10 -0600

    Added Lock Puzzle
    I did make a few changes to how it works tho

---
+++ TSA Vidoe Game Design/Assets/Levelload.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levelload : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Application.LoadLevel("End");
+        Debug.Log("Wow!");
+    }
+}
---
+++ TSA Vidoe Game Design/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+
+    //private Animator anim;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+       // anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+      // anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+        //transform.LookAt(moveVelocity);
+
+
+    }
+}
