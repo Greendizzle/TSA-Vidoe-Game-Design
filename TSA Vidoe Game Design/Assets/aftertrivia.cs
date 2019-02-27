0d6e9e2885cffd548bfaf3e402014f8c6b62ca6a
CHANGSAW <0texasstate0@protonmail.com>
2019-02-27 03:26:58 -0600

    fdsadfsa

---
+++ TSA Vidoe Game Design/Assets/aftertrivia.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aftertrivia : MonoBehaviour
+{
+    public void TransitionTo3()
+    {
+        Application.LoadLevel("3");
+    }
+}
