1f21ed4f7eaa650269d2e15e9669c8e98d5fdafe
CHANGSAW <0texasstate0@protonmail.com>
2019-02-27 23:33:32 -0600

    fdsa

--- TSA Vidoe Game Design/Assets/Scripts/Kill.cs
+++ TSA Vidoe Game Design/Assets/Scripts/Kill.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Kill : MonoBehaviour
 {
+    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Application.LoadLevel("Lock");
